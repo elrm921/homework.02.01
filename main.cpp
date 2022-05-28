1c9cef56ae003fe2b50ab29b47ece18c62d01b0e
elrm921 <pocketrocket03@gmail.com>
2022-05-28 10:53:37 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+enum month {
+    null,
+    january,
+    febrary,
+    marach,
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    november,
+    december
+};
+
+int main() {
+    while (true) {
+        int num = 0;
+        std::cout << "Введите номер месяца: ";
+        std::cin >> num;
+        month this_month = static_cast<month>(num);
+
+        switch (this_month) {
+            case 1:
+                std::cout << "Январь" << "\n";
+                break;
+            case 2:
+                std::cout << "Февраль" << "\n";
+                break;
+            case 3:
+                std::cout << "Март" << "\n";
+                break;
+            case 4:
+                std::cout << "Апрель" << "\n";
+                break;
+            case 5:
+                std::cout << "Май" << "\n";
+                break;
+            case 6:
+                std::cout << "Июнь" << "\n";
+                break;
+            case 7:
+                std::cout << "Июль" << "\n";
+                break;
+            case 8:
+                std::cout << "Август" << "\n";
+                break;
+            case 9:
+                std::cout << "Сентябрь" << "\n";
+                break;
+            case 10:
+                std::cout << "Октябрь" << "\n";
+                break;
+            case 11:
+                std::cout << "Ноябрь" << "\n";
+                break;
+            case 12:
+                std::cout << "Декабрь" << "\n";
+                break;
+            case 0:
+                std::cout << "До свидания" << "\n";
+                return 0;
+            default:
+                std::cout << "Неправильный номер!" << "\n";
+                break;
+        }
+    }
+}
